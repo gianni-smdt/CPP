9872f4c486c3f24a52e48e4329a581e6a7d517cd
Gianni <giannischmidt2001@gmail.com>
2025-01-18 21:05:34 +0100

    Create Dynamic Polymorphism.cpp

---
+++ Code Examples for Studies/Dynamic Polymorphism.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+struct Shape{ 
+    virtual void draw() const = 0; //pure virtual
+    virtual ~Shape() = default;
+};
+
+struct Square : Shape {
+    virtual void draw() const override final{
+        std::cout << "[ ]" << std::endl;
+    }
+};
+
+int main()
+{
+    std::unique_ptr<Shape> s = std::make_unique<Square>();
+
+    s->draw();
+
+    std::vector<std::unique_ptr<Shape>> shapes; 
+    shapes.push_back(std::make_unique<Square>());
+}
