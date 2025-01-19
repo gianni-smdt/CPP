a5bbe9dd56c60939cc2ba157c244d0c615a05834
Gianni <giannischmidt2001@gmail.com>
2025-01-19 12:22:46 +0100

    Update Pointers_and_References.cpp

--- Code Examples for Studies/Pointers_and_References.cpp
+++ Code Examples for Studies/Pointers_and_References.cpp
@@ -4,14 +4,14 @@
 int main(){
     uint8_t x {42}; //x is on the stack
 
-    const uint8_t xx = x; //xx is an independent copy of x
+    const uint8_t cx = x; //cx is an independent copy of x
 
     uint8_t &rx = x; //rx is a reference of x
 
     rx = 23;
 
     std::cout << "x: " << static_cast<int>(x) //23
-            << " xx: " << static_cast<int>(xx) //42
+            << " cx: " << static_cast<int>(cx) //42
             << " rx: " << static_cast<int>(rx) //23
             << std::endl;
 
@@ -21,7 +21,7 @@ int main(){
     *px = 8;
 
     std::cout << "x: " << static_cast<int>(x) //8
-            << " xx: " << static_cast<int>(xx) //42
+            << " cx: " << static_cast<int>(cx) //42
             << " rx: " << static_cast<int>(rx) //8
             << " *px: " << static_cast<int>(*px) //8
             << " Address of px: " << static_cast<void*>(px)
